f85fc46456950e4be50bbd29059c48989139f91f
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-11-30 19:14:01 +0100

    Zmiana imienia

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Natalia"
+moje_imie = "Paulina"
 msg = "Hello World!"
 
 @app.route('/')
@@ -17,3 +17,4 @@ def index():
 @app.route('/outputs')
 def supported_output():
     return ", ".join(SUPPORTED)
+
